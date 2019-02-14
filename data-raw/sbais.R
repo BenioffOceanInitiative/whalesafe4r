cd9882596375f693edcbd7cccec91f3d289e13fa
mvisalli <mvisalli@bren.ucsb.edu>
2019-02-14 14:04:51 -0800

    fixed #2

--- data-raw/sbais.R
+++ data-raw/sbais.R
@@ -4,7 +4,7 @@ sbais <- tibble::tibble(
   txt = list.files("data-raw", "^AIS_.*\\.txt$", full.names = T, recursive = T)) %>%
   dplyr::mutate(
     # note use of custom read_ais_txt() function applied to each *.txt file
-    data = purrr::map(txt, bbnj::read_ais_txt)) %>%
+    data = purrr::map(txt, shipr::read_ais_txt)) %>%
   dplyr::select(-txt) %>%
   tidyr::unnest(data) %>%
   dplyr::arrange(datetime, name)

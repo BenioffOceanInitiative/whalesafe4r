a1ecebf79fb35d3ee25506d3251d65b26476b69c
Ben Best <ben@ecoquants.com>
2019-11-01 17:21:38 -0400

    progressing with txt download and copy_to

--- R/postgres_connect.R
+++ R/postgres_connect.R
@@ -1,64 +1,20 @@
 # install.packages("RPostgreSQL")
-require("RPostgreSQL")
-install.packages("RPostgreSQL")
+# require("RPostgreSQL") # install.packages("RPostgreSQL")
 library(RPostgreSQL)
+library(dplyr)
+library(dbplyr)
 
-driver <- dbDriver("PostgreSQL")
 # creates a connection to the postgres database
+driver <- dbDriver("PostgreSQL")
 
-con <- dbConnect(driver,
-                 dbname = "mydb",
-                 host = "yew.clem64jg8sf4.us-west-1.rds.amazonaws.com",
-                 port = 5432,
-                 user = "master",
-                 password = "Kook1991")
+# echo "secret" > ~/private/boi_ship-strike_rds-free.txt
+pw_file <- "~/private/boi_ship-strike_rds-free.txt"
+pw <- readLines(pw_file)
 
-sql_command = "CREATE TABLE ais_data (
-            X1 TIMESTAMPZ,
-            X2 double precision,
-            X3 double precision,
-            X4 double precision,
-            X5 double precision,
-            X6 double precision,
-            X7 double precision,
-            X8 double precision,
-            X9 double precision,
-            X10 double precision,
-            X11 double precision,
-            X12 double precision,
-            X13 double precision,
-            X14 double precision,
-            X15 double precision,
-            X16 double precision,
-            X17 double precision,
-            X18 double precision,
-            X19 double precision,
-            X20 double precision,
-            X21 double precision,
-            X22 double precision,
-            X23 double precision,
-            X24 double precision,
-            X25 double precision
-            );"
-
-dbGetQuery(con, sql_command)
-
-hours.list = c(0:23)
-hours.list_1 = sprintf('%02d', hours.list)
-
-ais_text = lapply(paste0('https://ais.sbarc.org/logs_delimited/2019/190102/AIS_SBARC_190102-', hours.list_1,'.txt'),
-           function(url){
-             url %>%
-               read_delim(";", col_names = sprintf("x%d", 1:25), col_types = ais_col_types)
-
-           }
-  )
-  DF = do.call(rbind.data.frame, ais_text)
-
-
-
-dbWriteTable(con, "ais_data", value = DF, append=TRUE, row.names=FALSE)
-
-
-
-
+con <- dbConnect(
+  driver,
+  dbname = "mydb",
+  host = "yew.clem64jg8sf4.us-west-1.rds.amazonaws.com",
+  port = 5432,
+  user = "master",
+  password = pw)
--- R/scrape.R
+++ R/scrape.R
@@ -1,14 +1,26 @@
 library(xml2)
 library(magrittr)
-library(tidyverse)
+#library(tidyverse)
 library(readr)
+library(stringr)
 library(jsonlite)
+library(glue)
+library(progress)
+library(here)
+library(dplyr)
+
+# paths
+dir_gdata    <- "/Volumes/GoogleDrive/My Drive/projects/ship-strike/data/ais.sbarc.org/"
+url_ftp      <- "https://ais.sbarc.org/logs_delimited/"
+pg_connect_R <- here("R/postgres_connect.R")
+
+source(pg_connect_R)
 
 #get all urls with .txt
 .get_link <- function(u){
-  node <- xml2::read_html(u)
+  node  <- xml2::read_html(u)
   hrefs <- xml2::xml_find_all(node, ".//a[not(contains(@href,'../'))]") %>% xml_attr("href")
-  urls <- xml2::url_absolute(hrefs, xml_url(node))
+  urls  <- xml2::url_absolute(hrefs, xml_url(node))
   if(!all(tools::file_ext(urls) == "txt")){
     lapply(urls, .get_link)
   }else {
@@ -17,17 +29,72 @@ library(jsonlite)
 }
 
 #get all links from base url
-a <- .get_link("https://ais.sbarc.org/logs_delimited/")
+lst_links <- .get_link(url_ftp)
 
 #unlist
-b <- unique(unlist(a))
+links <- unique(unlist(lst_links))
+
+url <- links[1]
+
+download_url <- function(url){
+  txt <- str_replace(url, url_ftp, dir_gdata)
+  dir.create(dirname(txt), showWarnings = F, recursive = T)
+  if (!file.exists(txt)) download.file(url, txt)
+}
+#walk(b, download_url)
+
+n_links <- length(links)
+pb <- progress_bar$new(total = n_links)
+for (i in 1:n_links) { # i = 1
+  pb$tick()
+  Sys.sleep(1 / 100)
+
+  url <- links[i]
+  message(glue("{i} of {length(links)}: {url}"))
+  download_url(url)
+
+  #if (i == 10) browser()
+}
+
+# Next:
+#  - load individual txt files into database
+d <- shipr::read_ais_txt(txt)
+
+d$src_url <- url
+
+# TODO: check for txt already loaded
+d %>% group_by(src_url) %>% summarize(n = n())
+
+# TODO: check for parsing failures -- read_csv(n_max = nLinesOfTxt)
+#  - load into database
+#  - log files entered
+#View(d)
+#
+#cat(paste(names(d), collapse='","'))
+
+# initial copy
+dbplyr::copy_to(
+  con, d, "ais_data", overwrite = T, temporary = F,
+  indexes = c("datetime","name","ship_type","mmsi","speed","lon","lat","heading"))
+
+# subsequent append
+# TODO: check if already there, otherwise skip
+dbWriteTable(con, "ais_data", value = d, append=TRUE, row.names=FALSE)
+
+# subsequent querying
+tbl_ais <- dbplyr::tbl(con, "ais_data")
+
+d_bupkis <- tbl_ais %>%
+  select(datetime, lon, lat) %>%
+  filter(name == "bupkis") %>%
+  collect()
 
 #read data in
-a_dfs <- rbind_pages(lapply(b[sample(1:length(b),100)], function(i){
-  raw <- readLines(i)
-  Sys.sleep(0.1)
-  str_matrix <- stringi::stri_split_regex(raw, "\\;", simplify = TRUE)
-  as.data.frame(apply(str_matrix, 2, function(j){
-    ifelse(!nchar(j), NA, j)
-  })) %>% mutate(file_name = i)
-}))
+# a_dfs <- rbind_pages(lapply(b[sample(1:length(b),100)], function(i){
+#   raw <- readLines(i)
+#   Sys.sleep(0.1)
+#   str_matrix <- stringi::stri_split_regex(raw, "\\;", simplify = TRUE)
+#   as.data.frame(apply(str_matrix, 2, function(j){
+#     ifelse(!nchar(j), NA, j)
+#   })) %>% mutate(file_name = i)
+# }))

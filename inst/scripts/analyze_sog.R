a33c27384f77a2c4dbe49f9aa4de5bdc07e3438a
SeanGoral <seangoral@ucsb.edu>
2019-11-20 19:31:53 -0800

    viz compare

--- inst/scripts/analyze_sog.R
+++ inst/scripts/analyze_sog.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+
 library(sf)
 library(lubridate)
 library(units)
@@ -23,9 +24,11 @@ get_segment <- function(p1, p2, crs=4326){
     st_set_crs(crs)
 }
 
-pts <- sbais %>%
+df_postgres <- dbGetQuery(con, "SELECT datetime, name, mmsi, speed, lon, lat from all_ship_data WHERE datetime >= '2019-11-01'")
+
+pts <- df_postgres %>%
   # filter to single vessel
-  #group_by(name) %>%
+  filter(name == "OCEAN ANGEL 3") %>%
   # convert to sf points tibble
   st_as_sf(coords = c("lon", "lat"), crs=4326) %>%
   # sort by datetime
@@ -38,80 +41,53 @@ pts <- sbais %>%
     seg_mins = (datetime - lag(datetime)) %>% as.double(units = "mins"),
     seg_km   = map_dbl(seg, get_length_km),#giving warning
     seg_kmhr = seg_km / (seg_mins / 60),
+    seg_new  = if_else(is.na(seg_mins) | seg_mins > 60, 1, 0),
     #apply speed over ground to next segment
-    seg_sog = lag(speed),
-    #if speed over ground is NA or greaterthan 50, return 1, otherwise 0
-    seg_new  = if_else(is.na(seg_sog) | seg_sog > 50, 1, 0))
+    seg_sog = speed)
 
 # setup lines
 lns <- pts %>%
   filter(seg_km <=100) %>%
   filter(!is.na(seg_sog)) %>%
   filter(seg_new == 0) %>%
-  group_by(name) %>%
+  #group_by(name) %>%
   mutate(
     seg_geom = map(seg, 1) %>% st_as_sfc(crs=4326)) %>%
   st_set_geometry("seg_geom") %>%
   select(-seg, -geometry) %>%
   rename(geometry = seg_geom)
 
-#map to see linestring errors
-#ggplot map...
-g = ggplot(lns) +
-  annotation_map_tile(zoom = 7) +
-  geom_sf(aes(color = seg_sog)) +
-  coord_sf(xlim = c(-132, -110), ylim = c(37, 20))
-
-g
+lns$seg_sog = as.numeric(lns$seg_sog)
 
 #leaflet map for S.O.G.
-pal <- leaflet::colorNumeric("Spectral", lns$seg_sog, reverse=T)
+pal <- leaflet::colorNumeric(palette="Spectral", lns$seg_kmhr, reverse=T)
 
-data("providers", package="leaflet")
+leaflet::leaflet(lns) %>%
+  leaflet::addProviderTiles(providers$Esri.OceanBasemap) %>%
+  leaflet::addPolylines(
+    color = ~pal(seg_kmhr),
+    label = ~sprintf("%0.03f km/hr on %s", seg_kmhr, datetime)) %>%
+  leaflet::addLegend(
+    pal = pal, values = ~seg_kmhr, title = "Speed (km/hr)")
+
+
+#leaflet map for km/hr calculated by distance/time
+pal <- leaflet::colorNumeric(palette="Spectral", lns$seg_kmhr, reverse=T)
+
+pal1 <- leaflet::colorNumeric(palette="Spectral", lns$seg_sog, reverse=T)
 
 leaflet::leaflet(lns) %>%
   leaflet::addProviderTiles(providers$Esri.OceanBasemap) %>%
   leaflet::addPolylines(
     color = ~pal(seg_sog),
-    label = ~sprintf("%0.03f km/hr on %s", seg_sog, datetime)) %>%
+    label = ~sprintf("%0.03f km/hr on %s", seg_sog, datetime), group="sog") %>%
   leaflet::addLegend(
-    pal = pal, values = ~seg_sog, title = "Speed (km/hr)")
-
-pts_1 <- sbais %>%
-  # filter to single vessel
-  # convert to sf points tibble
-  st_as_sf(coords = c("lon", "lat"), crs=4326) %>%
-  # sort by datetime
-  arrange(datetime) %>%
-  # filter to only one point per minute to reduce weird speeds
-  filter(!duplicated(round_date(datetime, unit="minute"))) %>%
-  mutate(
-    # get segment based on previous point
-    seg      = map2(lag(geometry), geometry, get_segment),
-    seg_mins = (datetime - lag(datetime)) %>% as.double(units = "mins"),
-    seg_km   = map_dbl(seg, get_length_km),
-    seg_kmhr = seg_km / (seg_mins / 60),
-    seg_new  = if_else(is.na(seg_mins) | seg_mins > 60, 1, 0),
-    seg_num  = cumsum(seg_new))
-
-# setup lines
-lns_1 <- pts_1 %>%
-  filter(seg_new == 0) %>%
-  filter(seg_km <=100) %>%
-  group_by(name) %>%
-  mutate(
-    seg_geom = map(seg, 1) %>% st_as_sfc(crs=4326)) %>%
-  st_set_geometry("seg_geom") %>%
-  select(-seg, -geometry) %>%
-  rename(geometry = seg_geom)
-
-#leaflet map for km/hr calculated by distance/time
-pal1 <- leaflet::colorNumeric("Spectral", lns_1$seg_kmhr, reverse=T)
-
-leaflet::leaflet(lns_1) %>%
-  leaflet::addProviderTiles(providers$Esri.OceanBasemap) %>%
+    pal = pal1, values = ~seg_sog, title = "Speed (km/hr)") %>%
   leaflet::addPolylines(
-    color = ~pal1(seg_kmhr),
-    label = ~sprintf("%0.03f km/hr on %s", seg_kmhr, datetime))
-
-
+    color = ~pal(seg_kmhr),
+    label = ~sprintf("%0.03f km/hr on %s", seg_kmhr, datetime),group="speed_calc") %>%
+  addLayersControl(
+  #baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+  overlayGroups = c("sog", "speed_calc"),
+  options = layersControlOptions(collapsed = F)
+)

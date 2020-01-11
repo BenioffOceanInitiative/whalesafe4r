3e78d48724bf35191d805f3050f3302172d13263
SeanGoral <seangoral@ucsb.edu>
2020-01-10 18:57:32 -0800

    Update db with new ais data function.

---
+++ R/update_ais.R
@@ -0,0 +1,45 @@
+
+#' Update AIS data in postgres database
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' cred_path = "/Users/seangoral/github/s4wr/s4w_amazon_rds.yml"
+#'
+update_ais_data <- function(){
+  #create connection
+  con =db_connect()
+
+  log = dbGetQuery(con, "SELECT * FROM log_df;")
+  log = log %>% select(-row.names)
+
+  last_read = logfile.last_url(log)
+
+  new_links = get_ais_urls(last_read)
+
+  log_df <- rbind(log, data.frame(url = new_links, is_read = F, timestamp = as.numeric(Sys.time())))
+
+  #loop through new_links and update log_df
+  new_data <- purrr::map(new_links, function(url){
+    df <-  tryCatch(whale.read(path = url, log_df = log_df, assign_back = TRUE), error=function(e) NULL)
+    assign(url, df)
+  })
+
+  DF= do.call(rbind,new_data)
+
+  dbWriteTable(con, name = 'ais_data_testy', value = DF, append=T)
+
+  dbWriteTable(con, name = 'log_df_test', value = log_df, overwite=T)
+
+  dbDisconnect(con)
+
+  return(DF)
+
+  }
+
+
+system.time({
+test=update_ais_data()
+})
+

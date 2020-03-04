4bd9a9a796a650bf88c6f08c6cc4431fbd18b32c
SeanGoral <seangoral@ucsb.edu>
2020-03-04 04:38:41 +0000

    .fix.

--- R/update_ais.R
+++ R/update_ais.R
@@ -1,9 +1,8 @@
-#' Update AIS Data
+#' Get AIS Data
 #' 
 #' Use urls2df() to loop through links to create AIS dataframe  
 #' @description Uses urls2df() function to loop through ais.txt URLs list created by get_ais_urls() function. Creates ais_data data.frame which will write to the database.
-#' @return nais_data points data.frame
-#' @importFrom RPostgreSQL dbDisconnect dbWriteTable
+#' @return ais_data data.frame
 #' @importFrom parallel mclapply detectCores
 #' @importFrom data.table rbindlist
 #'
@@ -12,9 +11,8 @@
 #'
 #' @export
 
-update_ais_data <- function(links=NULL){
-#initiate database connection
-  con = db_connect()
+get_ais_data <- function(links=NULL){
+
 # Loop through new_links using urls2df() 
   ais_data <-  mclapply(links, function(url){
     df <-  tryCatch(urls2df(path = url), error=function(e) NULL)
@@ -23,47 +21,68 @@ update_ais_data <- function(links=NULL){
 
   # Row bind "ais_data" 
   ais_data = rbindlist(ais_data)
-
-  dbWriteTable(conn = con,
-               name = "test_ais_data",
-               value = ais_data,
-               append = TRUE)
-  
-  dbDisconnect(conn = con)
   
   return(ais_data)
 }
 
+#' Update AIS Database Table
+#'
+#' @param ais_data ais_data data.frame
+#' @importFrom RPostgreSQL dbDisconnect dbWriteTable
+#' @export
 
-#' Update Segments Data
+update_ais_data <- function(ais_data=NULL){
+  #initiate database connection
+  con = db_connect()
+  # Write AIS data to the ais_data database table
+  dbWriteTable(conn = con,
+               name = "test_ais_data",
+               value = ais_data,
+               append = TRUE)
+  # Disconnect from database
+  dbDisconnect(conn = con)
+}
+
+#' Get New AIS Segments Data
+#'
+#' @param ais_data 
+#'
+#' @return segs_data "sf" "data.table" "data.frame"
+#' @examples 
+#' new_segs_data = get_segment_data(ais_data = new_ais_data)
+#' @export
+#'
+get_segment_data <- function(ais_data=NULL){
+  
+  segs_data = ais2segments(ais_data)
+  
+  segs_data$year = as.integer(segs_data$year)
+  
+  return(segs_data)
+}
+
+#' Update AIS Segments Database Table
 #'
 #' @param ais_data
 #'
-#' @return segs_data "sf" "data.table" "data.frame"
 #' @importFrom RPostgreSQL dbDisconnect dbWriteTable
-#' @description Creates database connection, produces new ais segments data frame from ais_data, and writes it to the "ais_segments" database table.
+#' @description Creates database connection and writes new ais segments data frame to the "ais_segments" database table.
 #' 
 #' @examples
-#'   new_segs_data = update_segments_data(ais_data = new_ais_data_test)
 #' 
 #' @export
 
 update_segments_data <- function(ais_data=NULL){
 # Initiate connection for temporary table session
   con = db_connect()
-# Run new_ais_data through ais2segments() function ----
-  segs_data = ais2segments(ais_data)
-  
-  segs_data$year = as.integer(segs_data$year)
   
   dbWriteTable(conn = con, 
-               name = "test_ais_segments", 
+               name = "ais_segments", 
                value = segs_data, 
                append = TRUE)
   
   dbDisconnect(conn = con)
   
-  return(segs_data)
 }
 
 #' Get IHS Data
@@ -94,13 +113,14 @@ get_ihs_data <- function(){
 #' @examples
 #' update_vsr_segments(segs_data = new_segs_data)
 #' @export
+
 update_vsr_segments <- function(segs_data=NULL){
   # Initiate database connection
   con = db_connect()
   # Write 'segs_data' to a temporary table called "temp_ais_segments"
   dbWriteTable(conn = con,
                name = "temp_ais_segments",
-               value = segs_data,
+               value = new_segs_data,
                temporary = TRUE)
   # SQL'vsr_query' creates temporary "temp_vsr_segments" table which intersects vsr_zones based on date ranges ----
   vsr_query = ("CREATE TEMPORARY TABLE 
@@ -133,38 +153,76 @@ update_vsr_segments <- function(segs_data=NULL){
   # snag temp_vsr_segments just for fun ----
   temp_vsr_segments = dbGetQuery(conn = con, "SELECT * FROM temp_vsr_segments;")
   
-  # Left join query to join vsr_segments data and ihs_data
-  join_query = "CREATE TEMPORARY TABLE 
-                temp_vsr_ihs_segs AS
-                SELECT temp_vsr_segments.*, ihs_data.*
-                FROM temp_vsr_segments 
-                LEFT JOIN ihs_data ON 
-                temp_vsr_segments.mmsi = ihs_data.mmsi_ihs"
-  
-  dbExecute(conn = con, join_query)
-   # Insert temporary table "temp_vsr_ihs_segs" into "vsr_segments' table.
+  # # Left join query to join vsr_segments data and ihs_data
+  # join_query = "CREATE TEMPORARY TABLE 
+  #               temp_vsr_ihs_segs AS
+  #               SELECT temp_vsr_segments.*, ihs_data.*
+  #               FROM temp_vsr_segments 
+  #               LEFT JOIN ihs_data ON 
+  #               temp_vsr_segments.mmsi = ihs_data.mmsi_ihs"
+  # 
+  # dbExecute(conn = con, join_query)
+  #  # Insert temporary table "temp_vsr_ihs_segs" into "vsr_segments' table.
   dbExecute(conn = con, 
             "INSERT INTO vsr_segments
-            SELECT * FROM temp_vsr_ihs_segs;")
+            SELECT * FROM temp_vsr_segments;")
   
   # Disconnect from temporary table connection
   dbDisconnect(conn = con)
 
 }
 
-get_vsr_ihs_segs_data <- function(){
 
-con = db_connect()
+#' Get VSR and IHS data 
+#'
+#' @return vsr_segs_ihs 
+#' @examples
+#' vsr_ihs_data = get_vsr_ihs_data()
+#' @export
 
-vsr_ihs_segs_query= "SELECT * FROM vsr_segments 
-                    WHERE 'operator' IS NOT NULL;"
-vsr_ihs_segs_data = dbGetQuery(con, vsr_ihs_segs_query)
-
-dbDisconnect(con)
-
-return(vsr_ihs_segs_data)
+get_vsr_ihs_data <- function(){
+  # Connect to DB
+  con=db_connect()
+  # Get vsr_gegments data from database
+  vsr_segs = tbl(con,"vsr_segments") %>% select(-geometry)
+  # Get IHS data from database
+  ihs_data = tbl(con, "ihs_data")
+  # Merge/inner join vsr_segments and IHS data to get only segments with complete records...
+  vsr_segs_ihs = merge(x=vsr_segs, y=ihs_data, 
+                       by="mmsi", all.x = TRUE)
+  # Create 'date' column 
+  vsr_segs_ihs$date = as.Date(format(vsr_segs_ihs$beg_dt,"%Y-%m-%d"))
+  # set the data frame as data table
+  # vsr_segs_ihs = setDT(vsr_segs_ihs)
+  dbDisconnect(con)
+  
+  return(vsr_segs_ihs)
 }
 
 system.time({
-vsr_ihs_segs_data <- get_vsr_ihs_segs_data()
+vsr_ihs_data = get_vsr_ihs_data()
 })
+# user  system elapsed 
+# 36.997   5.884  91.413 
+# 
+
+# get_vsr_ihs_segs_data <- function(){
+#   
+#   con = db_connect()
+#   
+#   vsr_ihs_segs_query= "SELECT * FROM vsr_segments 
+#   WHERE 'operator' IS NOT NULL;"
+#   vsr_ihs_segs_data = dbGetQuery(con, vsr_ihs_segs_query)
+#   
+#   dbDisconnect(con)
+#   
+#   return(vsr_ihs_segs_data)
+# }
+
+# system.time({
+# vsr_ihs_segs_data <- get_vsr_ihs_segs_data()
+# })
+
+# user  system elapsed 
+# 46.708   7.840 107.405 
+
